b64de0045ef170a2df0f7deecca3653425c8eb74
Admin <sdfsdf@sdfsd>
2025-01-31 03:08:08 +0200

    added card in InformationPurchaseScreen

--- app/src/main/java/com/example/coffeeshop/screens/Administrator/InformationPurchaseScreen.kt
+++ app/src/main/java/com/example/coffeeshop/screens/Administrator/InformationPurchaseScreen.kt
@@ -1,14 +1,30 @@
 package com.example.coffeeshop.screens.Administrator
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.breens.beetablescompose.BeeTablesCompose
 
 
 @Composable
 fun InformationPurchaseScreen() {
-
+    InformationPurchase(supplierDataList[0])
 }
 
 
@@ -18,22 +34,59 @@ fun PreviewInformationPurchaseScreen(){
     InformationPurchaseScreen()
 }
 
-
 @Composable
 fun InformationPurchase(
     supplier: Supplier
 ){
-    Column {
-        Text(
-            text = "Інформація"
-        )
-        Text("""
-            |Телефон: ${supplier.phoneNumber}
-            |Пошта: ${supplier.email}
-            """.trimMargin()
-        )
 
+    LazyColumn {
+        item {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Інформація",
+                        style = MaterialTheme.typography.bodySmall,
+                        fontSize = 25.sp
+                    )
+                    Spacer(modifier = Modifier.height(20.dp))
+                    Column(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.Start
+                    ) {
+                        Text(
+                            text = supplier.nameCompany,
+                            style = MaterialTheme.typography.bodySmall,
+                            fontSize = 20.sp
+                        )
+                        Text(
+                            text = "Телефон: ${supplier.phoneNumber}",
+                            style = MaterialTheme.typography.bodySmall,
+                            fontSize = 20.sp
+                        )
+                        Text(
+                            text = "Пошта: ${supplier.email}",
+                            style = MaterialTheme.typography.bodySmall,
+                            fontSize = 20.sp
+                        )
+                    }
+                }
+            }
 
-
-    }
-}
+            val headerTitles = listOf("Назва", "Ціна", "Кількість")
+                    BeeTablesCompose(
+                        data = supplierDataList,
+                        headerTableTitles = headerTitles,
+                    )
+                }
+            }
+        }
--- app/src/main/java/com/example/coffeeshop/screens/Administrator/PurchaseScreen.kt
+++ app/src/main/java/com/example/coffeeshop/screens/Administrator/PurchaseScreen.kt
@@ -1,6 +1,9 @@
 package com.example.coffeeshop.screens.Administrator
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -23,12 +26,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -37,6 +42,7 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.coffeeshop.AppRoute
 import com.example.coffeeshop.R
 import com.example.coffeeshop.screens.componentsMenuScreen.ChipGroup
 import com.example.navigationmodule.LocalRouter
@@ -119,18 +125,27 @@ fun CardSupplier(
     val router = LocalRouter.current
     Card(
         modifier = Modifier.fillMaxWidth()
-            .padding(10.dp)
-            .size(100.dp)
-            .clip(CircleShape),
-        shape = RoundedCornerShape(10)
+            .padding(5.dp)
+            .clickable {
+                router.launch(AppRoute.Administrator.Purchase.InformationPurchase)
+            },
+        shape = RoundedCornerShape(50)
     ) {
         Row{
-            Image(
-                painter =supplier.photo ?: painterResource(R.mipmap.face_photo),
-                contentDescription = null,
-                modifier = Modifier.size(100.dp)
-                    .weight(1f)
-            )
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(CircleShape)
+                    .border(2.dp, Color.Gray, CircleShape)
+            ){
+                Image(
+                    painter =supplier.photo ?: painterResource(R.mipmap.face_photo),
+                    contentDescription = null,
+                    modifier = Modifier.size(100.dp),
+                    contentScale = ContentScale.Crop
+                )
+            }
             Column(
                 modifier = Modifier.weight(2f)
                     .padding(start = 10.dp, top = 5.dp)
@@ -154,12 +169,18 @@ val supplierDataList = listOf(
         email = "kurva@gnal.com",
         phoneNumber = "234234234234",
         products = listOf(
-            Product("картоїа", "eerteg", 1231.0),
-            Product("dfgdf", "sdfsfss", 131.0),
-            Product("картоdfgdїа", "sfdsfs", 121.0),
-            Product("dgfdgd", "fsdfs", 111.0)
+            Product("Еспресо", "Кава", 50.0),
+            Product("Капучино", "Кава", 70.0),
+            Product("Лате", "Кава", 75.0),
+            Product("Американо", "Кава", 60.0),
+            Product("Торт Чизкейк", "Десерт", 90.0),
+            Product("Круасан", "Випічка", 40.0),
+            Product("Маффін з ягодами", "Випічка", 50.0),
+            Product("Тістечко Шоколадне", "Десерт", 60.0),
+            Product("Фреш з апельсина", "Напої", 45.0),
+            Product("Чай зелений", "Напої", 35.0)
         ),
-        photo = null /* Ваше зображення тут */
+        photo = null
     )
 )
 data class Supplier(
