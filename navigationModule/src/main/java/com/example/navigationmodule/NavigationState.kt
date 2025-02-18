d21a8db40ba3d2da716c611ec244507199c0bb22
GunderukVolodymyr <volodymyr.gunderuk@kpk-lp.com.ua>
2025-02-18 14:28:00 +0200

    delete

--- navigationModule/src/main/java/com/example/navigationmodule/NavigationState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.navigationmodule
-
-import androidx.compose.runtime.Stable
-
-@Stable
-interface NavigationState {
-    val isRoot:Boolean
-    val currentRoute: Route
-}
--- navigationModule/src/main/java/com/example/navigationmodule/Route.kt
+++
@@ -1,6 +0,0 @@
-package com.example.navigationmodule
-
-import androidx.compose.runtime.Immutable
-
-@Immutable
-interface Route
